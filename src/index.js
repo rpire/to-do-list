3735b849ce0a3ae194791373128518b021516875
rpire <rubenpire7@gmail.com>
2021-08-17 11:32:28 -0400

    Set up repository with webpack

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import _ from 'lodash';
+import printMe from './print.js';
+import './style.css';
+
+function component() {
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
+
+  element.innerHTML = _.join(['Hello', 'world!'], ' ');
+  element.classList.add('hello');
+   
+  btn.innerHTML = 'Click here to be insulted aggressively';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  return element;
+}
+
+document.body.appendChild(component());
---
+++ src/print.js
@@ -0,0 +1,3 @@
+export default function printMe() {
+  console.log('Eres sendo mamagüevo...');
+}
