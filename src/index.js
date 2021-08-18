92ab2cb26b86e6458d0a1aecd957451b559c7416
rpire <rubenpire7@gmail.com>
2021-08-18 13:51:04 -0400

    Apply second fix for ESLint errors

--- src/index.js
+++ src/index.js
@@ -37,9 +37,8 @@ const reload = () => {
   }
   const checkboxes = document.querySelectorAll('.checkbox');
   for (let i = 0; i < checkboxes.length; i += 1) {
-    checkboxes[i].addEventListener('change', () => {
-      const storage = checkComplete(listArr);
-      localStorage.setItem('toDoList', storage);
+    checkboxes[i].addEventListener('change', function() {
+      localStorage.setItem('toDoList', checkComplete(listArr));
       reload();
     });
   }
